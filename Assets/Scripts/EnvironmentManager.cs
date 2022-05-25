19f1d8c061fea3f592fec59a68257a6dffddb771
raassh-23 <muh.nur.abdurrauf@gmail.com>
2022-05-26 01:12:44 +0700

    add agent

---
+++ Assets/Scripts/EnvironmentManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    [SerializeField]
+    private SpaceshipController[] spaceshipControllers;
+    public int maxStep = 20000;
+    public int maxLap = 3;
+
+    private int timer = 0;
+
+    public bool isTraining = true;
+
+    private void Start()
+    {
+        foreach (var spaceshipController in spaceshipControllers)
+        {
+            spaceshipController.onFinishedRace.AddListener((winner) =>
+            {
+                if (isTraining)
+                {
+                    endEpisodeForAll(winner);
+                }
+            });
+        }
+        Debug.Log("EnvironmentManager Start");
+    }
+
+    private void FixedUpdate() {
+        if (isTraining) {
+            timer++;
+            if (timer >= maxStep) {
+                timer = 0;
+                endEpisodeForAll();
+            }
+        }
+    }
+
+    private void endEpisodeForAll(SpaceshipController winner = null) {
+        if (winner == null) {
+            Debug.Log("End episode, timer reached max step");
+        } else {
+            Debug.Log("End episode, " + winner.name + " won");
+        }
+
+        foreach (SpaceshipController spaceshipController in spaceshipControllers) {
+            if (spaceshipController == winner) {
+                spaceshipController.AddReward(1f);
+                spaceshipController.EndEpisode();
+            } else {
+                spaceshipController.AddReward(-1f);
+                spaceshipController.EpisodeInterrupted();
+            }
+        }
+    }
+}
